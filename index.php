13479d15bc2511de8eec61722958b23323b8d21c
backtheryophag <78556114+backtheryophag@users.noreply.github.com>
2022-11-21 11:54:34 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>main</title>
+</head>
+<body>
+	<div class="main">
+		<form method="post" target="#" class="form">
+      <H1> мой сайт на GitHub</h1>
+		</form>
+	</div>
+</body>
+</html>
